a7c254f2649ebf3dc1551a0ce6b0a68b87478618
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2023-01-18 20:17:15 +0530

    Timer for shuffling cards

--- CardWorkout/CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardWorkout/CardSelectionVC.swift
@@ -13,13 +13,24 @@ class CardSelectionVC: UIViewController {
     
     @IBOutlet var buttons: [UIButton]!
     
+    var cards : [UIImage] = Cards.AllValues
+    
+    var timer : Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        startTimer()
         for button in buttons {
             button.layer.cornerRadius = 8
         }
-
+    }
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage(){
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
     }
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
