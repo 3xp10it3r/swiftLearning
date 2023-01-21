6421daa5e2bc2a3ab939ac470c64b26f2b33f89d
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2023-01-21 21:28:56 +0530

    simple UI

--- BarChart-SwiftUI/BarChart-SwiftUI/ContentView.swift
+++ BarChart-SwiftUI/BarChart-SwiftUI/ContentView.swift
@@ -6,11 +6,41 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
+    
+    let chartData: [cellData] = [
+        .init(date: Date.from(year:2023,month:08,day:20), viewsCount: 50000),
+        .init(date: Date.from(year:2023,month:07,day:20), viewsCount: 60000),
+        .init(date: Date.from(year:2023,month:06,day:20), viewsCount: 55000),
+        .init(date: Date.from(year:2023,month:05,day:20), viewsCount: 80000),
+        .init(date: Date.from(year:2023,month:04,day:20), viewsCount: 90000),
+        .init(date: Date.from(year:2023,month:03,day:20), viewsCount: 50000),
+        .init(date: Date.from(year:2023,month:02,day:18), viewsCount: 100000),
+        .init(date: Date.from(year:2023,month:01,day:20), viewsCount: 60000)
+    ]
+    
     var body: some View {
         VStack {
-            Text("Hello, world!")
+            Chart{
+                RuleMark(y: .value("Goal", 70000))
+                    .foregroundStyle(Color.mint.gradient)
+                    .lineStyle(StrokeStyle(lineWidth: 1, dash: [5]))
+                    .annotation(alignment: .leading) {
+                        Text("Goal")
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                        
+                    }
+                
+                ForEach(chartData) { cell in
+                    BarMark(x: .value("Month", cell.date, unit: .month)
+                            ,y: .value("Views", cell.viewsCount))
+                }
+            }
+            .frame(height: 180)
+            .foregroundStyle(Color.pink.gradient)
         }
         .padding()
     }
@@ -23,10 +53,10 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
-struct chartData: Identifiable {
-    var id  = UUID()
-    var date : Date
-    var viewsCount : Int
+struct cellData: Identifiable {
+    let id  = UUID()
+    let date : Date
+    let viewsCount : Int
 }
 
 
--- BarChart-SwiftUI/BarChart-SwiftUI/Date+Ext.swift
+++ BarChart-SwiftUI/BarChart-SwiftUI/Date+Ext.swift
@@ -10,7 +10,7 @@ import Foundation
 
 extension Date {
     static func from(year:Int, month:Int , day:Int) -> Date {
-        var components = DateComponents(year:year, month:month, day: day)
+        let components = DateComponents(year:year, month:month, day: day)
         return Calendar.current.date(from:components)!
     }
 }
