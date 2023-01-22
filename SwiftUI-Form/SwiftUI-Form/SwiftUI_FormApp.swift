6715b8d307b69fb1846a2e8a669bcd774c4ecfea
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2023-01-23 01:21:02 +0530

    Initial changes

---
+++ SwiftUI-Form/SwiftUI-Form/SwiftUI_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FormApp.swift
+//  SwiftUI-Form
+//
+//  Created by Digvijay Gupta on 23/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
