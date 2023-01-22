6675db67441998428b6bf0dfd179b9a52c936b89
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2023-01-23 02:16:41 +0530

    form color picker date picker and navigation view and toolbar

--- SwiftUI-Form/SwiftUI-Form/ContentView.swift
+++ SwiftUI-Form/SwiftUI-Form/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName  = ""
+    @State private var birthDate = Date()
+    @State private var numberOfLikes = 1
+    @State private var shouldSendNewsLetter = false
+    @State private var circleColour : Color = .red
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+           
+            Form {
+                
+                
+                Section(header: Text("Personnal Information")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name",text: $lastName)
+                    DatePicker("BirthDate", selection: $birthDate, displayedComponents: .date)
+                }
+                
+                
+                
+                Section(header: Text("Action")) {
+                    Toggle("Send Newsletter", isOn: $shouldSendNewsLetter)
+                        .toggleStyle(SwitchToggleStyle(tint: .red))
+                    Stepper("Number of Likes", value: $numberOfLikes, in: 1...100)
+                    Text("My account has \(numberOfLikes) likes")
+                    
+                    Link("Terms of Service", destination: URL(string: "https://google.com")!)
+                }
+                
+                Section(header: Text("Color")) {
+                    Circle()
+                        .foregroundColor(circleColour)
+                        .frame(width:50,height: 50)
+                    ColorPicker("Choose Color", selection: $circleColour)
+                }
+                
+            }
+            .navigationTitle("Account")
+        
+            .toolbar {
+                ToolbarItemGroup {
+                    Button {
+                        hideKeyboard()
+                    } label: {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                    }
+                    
+                    Button("Save",action: saveUser)
+                }
+            }
+            
+            
         }
-        .padding()
+        .accentColor(.red)
+        
+    }
+    
+    func saveUser() {
+        print("Saved User!")
     }
 }
 
@@ -24,3 +77,12 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard(){
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
