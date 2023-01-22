70af263ff1507acbe17fa84c91e375ef789e840a
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2023-01-23 02:27:57 +0530

    Initial setup

---
+++ SwiftUI-Redacted/SwiftUI-Redacted/SwiftUI_RedactedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RedactedApp.swift
+//  SwiftUI-Redacted
+//
+//  Created by Digvijay Gupta on 23/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RedactedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
